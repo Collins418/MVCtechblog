61b9a9b50ebc26585f0bc3faf8fd6394e0c489a1
Collins418 <106499144+Collins418@users.noreply.github.com>
2022-10-18 12:24:52 -0500

    add routes to homepage and dashboard

---
+++ controllers/index.js
@@ -0,0 +1,4 @@
+const router = require('express').Router();
+const routesapi = require('./api');
+const routesHomepage = require('./routesHomepage');
+const routesDashboard = require('./routesDashboard');
