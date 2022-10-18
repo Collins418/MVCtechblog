7595c30af25a7b015a7c638cb3a87782600aeffa
Collins418 <106499144+Collins418@users.noreply.github.com>
2022-10-18 12:53:58 -0500

    add db,models-blog,comment,user,index

---
+++ models/blog.js
@@ -0,0 +1,53 @@
+//add connection to sequelize
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+// WHEN I click on an existing blog 
+// THEN I am presented with the post title, contents, post creator’s username, and date created for that post and have the option to leave a comment
+
+class Blog extends Model { }
+
+Blog.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        date_updated: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "blog",
+    }
+);
+
+module.exports = Blog;
